cc5006f41dbfa4e6d7ecd842700ee91f77d4cf38
KIMBITNA <twinkle@godo.co.kr>
2022-05-24 17:54:24 +0900

    #1 중간커밋

--- vuejs-a/src/App.vue
+++ vuejs-a/src/App.vue
@@ -1,15 +1,42 @@
 <template>
+
+
+  <div class="menu">
+    <a v-for="아무렇게나작명 in 메뉴들" :key="아무렇게나작명">{{아무렇게나작명}}</a>
+  </div>
+
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <a v-for="menu in 3" :key="menu">home</a>
+
+
+  <div>
+    <h4>{{products[0]}}</h4>
+    <p>{{price[0]}}</p>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>{{price[1]}}</p>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>{{price[2]}}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      메뉴들 : ['home', 'shop', 'about'],
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      price : [100, 200, 300],
+    };
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -22,5 +49,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu{
+  background:darkslateblue;
+  padding:15px;
+  border-radius:5px;
+}
+.menu a{
+  color:#fff;
+  padding:10px;
 }
 </style>
