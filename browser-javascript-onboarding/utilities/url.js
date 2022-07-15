51eb90572f485f3a5fbc7cea9d9927faa8b3aeca
KIMBITNA <twinkle@godo.co.kr>
2022-07-15 15:43:52 +0900

    feat : 브랜치 정리

--- browser-javascript-onboarding/utilities/url.js
+++
@@ -1,8 +0,0 @@
-export const getSearchParams = object => {
-  return new URLSearchParams(object).toString();
-}
-
-export const getSearchParam = key => {
-  const searchParams = new URLSearchParams(location.search);
-  return searchParams.get(key)
-}
